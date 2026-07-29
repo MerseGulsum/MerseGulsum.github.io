3e8e1faa68147c182d507f593c66509844e1e40c
MerseGulsum <gulsumkocan.123@gmail.com>
2026-07-29 03:57:38 +0300

    Enable static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+
   distDir: process.env.NODE_ENV === "production" ? ".next-build" : ".next",
+
   images: {
-    formats: ["image/avif", "image/webp"]
-  }
+    formats: ["image/avif", "image/webp"],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
