7134d7bf62d997e9d571c532d4a38d77438df970
MerseGulsum <gulsumkocan.123@gmail.com>
2026-07-29 04:00:12 +0300

    Fix static export output directory

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   output: "export",
   trailingSlash: true,
 
-  distDir: process.env.NODE_ENV === "production" ? ".next-build" : ".next",
-
   images: {
     formats: ["image/avif", "image/webp"],
     unoptimized: true,
